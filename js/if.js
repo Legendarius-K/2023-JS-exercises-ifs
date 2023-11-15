7b22191ff958506094def051ad4a9f1ebc09054f
Alexandra Blomberg <Alexandra@users-mbp.home>
2023-11-15 20:25:15 +0100

    complete exercise 4

--- js/if.js
+++ js/if.js
@@ -12,15 +12,30 @@ Eg:
 let exercise1 = prompt("Enter a number");
 */
 
-let exercise1 = prompt("Enter any number between 1-100");
+// EXERCISE 1, 2 AND 3:
+// let exercise1 = prompt("Enter any number between 1-100");
 
-if (exercise1 > 5) {
-    alert("Your number is greater than five!");
-} else if (exercise1 < 5) {
-    alert("Your number is less than five!");
-} else if (exercise1 == 5) {
-    alert("Your number is equal to five!");
-}
+// if (exercise1 > 5) {
+//     alert("Your number is greater than five!");
+// } else if (exercise1 < 5) {
+//     alert("Your number is less than five!");
+// } else if (exercise1 == 5) {
+//     alert("Your number is equal to five!");
+// }
+
+// EXERCISE 4:
+
+let userNumber = prompt("Enter any number");
+
+console.log(10 * userNumber);
+
+let secondUserNumber = prompt("Enter a new number");
+
+console.log(typeof secondUserNumber);
+
+let thirdUserNumber = prompt("Enter a third number");
+
+console.log(typeof (parseInt (thirdUserNumber)));
 
 
 });
