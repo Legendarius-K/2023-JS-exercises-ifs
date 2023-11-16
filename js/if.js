3fba93e3c1a842e1df2d004352da7181dc2a0996
Darius K <darius.kaya@gmail.com>
2023-11-16 11:20:58 +0100

    complete exercise 6-8

--- js/if.js
+++ js/if.js
@@ -28,17 +28,36 @@ let exercise1 = prompt("Enter a number");
 // console.log(typeof parseInt(exercise4))
 
 
-let exercise5 = prompt ("Enter a number!!");
+// let exercise5 = prompt ("Enter a number!!");
 
-if (exercise5 < 5) {
-    console.log(exercise5 + " is not a number between 5 and 10.")
-} else if (exercise5 > 10) {
-    console.log(exercise5 + " is not a number between 5 and 10.")
-} else {
-    console.log(exercise5 + " is a number between 5 and 10!")
-}
+// if (exercise5 < 5) {
+//     console.log(exercise5 + " is not a number between 5 and 10.")
+// } else if (exercise5 > 10) {
+//     console.log(exercise5 + " is not a number between 5 and 10.")
+// } else {
+//     console.log(exercise5 + " is a number between 5 and 10!")
+// }
 
+// let exercise7 = prompt ("Enter a number!!");
 
+// if (exercise7 < 5) {
+//     console.log(exercise7 + " is less than 5")
+// } else if (exercise7 > 10) {
+//     console.log(exercise7 + " is more than 10")
+// } else {
+//     console.log(exercise7 + " is neither less than 5 nor more than 10")
+// }
+
+// let exercise8_1 = prompt("Enter a number!")
+// let exercise8_2 = prompt("Enter another number!")
+
+// if (exercise8_1 > exercise8_2) {
+//     console.log(exercise8_1 + " is more than " + exercise8_2)
+// } else if (exercise8_2 > exercise8_1) {
+//     console.log(exercise8_2 + " is more than " + exercise8_1)
+// } else {
+//     console.log(exercise8_2 + " is equal to " + exercise8_1)
+// }
 
 
 });
