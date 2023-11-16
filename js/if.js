e430e77c7a66b99e72cd40ac5837a9050138cf5c
Davide Baldi <d.baldi88@gmail.com>
2023-11-16 11:26:23 +0100

    Exercise 9 DONE

--- js/if.js
+++ js/if.js
@@ -108,7 +108,7 @@ if (!userNumber) {
     }
 }
 */
-// EXERCISE 8 -ask the user to enter two numbers and show which is larger
+/* EXERCISE 8 -ask the user to enter two numbers and show which is larger
 let numberOne=parseFloat(prompt("Please insert the FIRST number"));
 let numberTwo=parseFloat(prompt("Please insert the SECOND number"));
 if (!numberOne || !numberTwo) {
@@ -125,3 +125,62 @@ if (!numberOne || !numberTwo) {
         document.querySelector(".exercise-8").textContent=numberOne + " is equal " + numberTwo;
     }
 }
+*/
+/*EXERCISE 9: ask the user to enter three different numbers and show them in order from largest to smallest in the same alert/0 is undefined???+ maggiore uguale
+let num1=parseFloat(prompt("Please insert FIRST number"));
+let num2=parseFloat(prompt("Please insert SECOND number"));
+let num3=parseFloat(prompt("Please insert THIRD number"));
+
+let numLarge, numMedium, numSmall;
+
+if (!num1 ||!num2 || !num3) {
+    console.log("at least one input is INVALID or EMPTY")
+} else {
+    if (num1>num2 && num1>num3) {
+        numLarge= num1;
+        if (num2>num3) {
+            numMedium= num2;
+            numSmall= num3;
+        } else {
+            numMedium= num3;
+            numSmall= num2;
+        }
+
+    } else if (num2>num1 && num2>num3) {
+        numLarge= num2;
+        if (num1>num3) {
+            numMedium= num1;
+            numSmall= num3;
+        } else {
+            numMedium= num3;
+            numSmall= num2;
+        }
+
+    } else if (num3>num1 && num3>num2) {
+        numLarge= num3;
+        if (num1>num2) {
+            numMedium= num1;
+            numSmall= num2;
+        } else {
+            numMedium= num2;
+            numSmall= num1;
+        }
+    }
+
+}
+
+alert("The following numbers are displayed from larger to smaller: "+ numLarge + " => " + numMedium + " => " + numSmall);
+*/
+
+/*EXERCISE 10: ask the user whether they are a cat or a dog person. QUESTION
+
+If they like cats, ask them if they prefer a tabby or a tiger QUESTION
+
+If they like dogs, ask them if they prefer a chihuahua or a wolf QUESTION - 4 CHOICHES
+
+A tabby says "miaow", tigers "growl", a chihuahua says "yap" and a wolf likes to "howl".
+
+Show the user their chosen breed and what it says
+
+Note: both you & the user will have to spell the breeds correctly for this to work 
+*/
