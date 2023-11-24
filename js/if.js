c6f196253e2f1878596ba1db2f3c05834551222c
Davide Baldi <d.baldi88@gmail.com>
2023-11-24 10:48:44 +0100

    EX2 EX3 TERNARY DONE

--- js/if.js
+++ js/if.js
@@ -13,6 +13,15 @@ let exercise1 = prompt("Enter a number");
 */
 
 });
-//Exercise 1: ask the user to enter a number and show if it is greater than 5
+/*Exercise 1: ask the user to enter a number and show if it is greater than 5
 let userNumber= parseInt(prompt("please insert a number"));
 (userNumber> 5) ? console.log(userNumber + " is greater than 5"): console.log("not higher than 5");
+*/
+/*Exercise 2: ask the user to enter a number and show if it is greater or less than 5
+
+let userNumber2= parseInt(prompt("CHECK IF NUMBER IS greater or equal"));
+(userNumber2>=5) ? console.log(userNumber2 + " is equal or greatern than 5"): console.log(userNumber2 + " is  NOT equal or greatern than 5");
+*/
+//Exercise 3: ask the user to enter a number and show if it is greater than, less than or equal to 5
+let numberInput= parseInt(prompt("insert a number"));
+(numberInput>5) ? console.log(numberInput + " is greater than 5"): (numberInput<5) ? console.log(numberInput + " is NOT greater than 5"): console.log(numberInput + " is EQUAL to 5");
